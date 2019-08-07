deb1976391f6b7ee24ae267539154e886276a2a1
yeyuanLin <15797731292@163.com>
2019-08-07 19:10:58 +0800

    ...

--- src/components/WriteComment.vue
+++ src/components/WriteComment.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="writeComment">
-    <mt-search placeholder="搜索想点评的电影" v-model="value"></mt-search>
+    <div class="search-field">
+      <i class="iconfont icon-sousuo"></i>
+      <input class="search" type="text" placeholder="搜索想点评的电影"/>
+    </div>
+<!--    <mt-search placeholder="搜索想点评的电影"></mt-search>-->
     <span>邀您点评</span>
     <div class="film-list">
-      <img class="film-img" :src="require('../assets/images/nz.jpg')">
+      <img class="film-img" :src="require('../assets/images/nz.jpg')"/>
       <div class="details">
         <h4 class="film-name">哪吒之魔童降世</h4>
         <span class="desc">天地灵气孕育出了一颗能量巨大的混元珠，原始天尊将混元珠提炼出来，其中仙气化作灵珠，魔气化作魔丸，天尊将它们封存于宝莲之中。</span>
@@ -29,10 +33,41 @@
   }
 </script>
 <style scoped>
-  .mint-search {
-    height: 55px;
+  .writeComment {
+    display: flex;
+    flex-direction: column;
   }
-  .mint-searchbar {
-    padding:5px 10px;
+  .search-field {
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    background-color: #e6e6e6;
+
   }
+  .search-field i {
+    position: absolute;
+    top: 6px;
+    left:5px;
+  }
+  .search {
+    width: 320px;
+    height: 30px;
+    /*border:1px solid #ccc;*/
+    border:none;
+    border-radius: 4px;
+    outline: none;
+    background-color: #fff;
+    margin-top: 3px;
+  }
+
+
+
+
+
+
+
+
+
+
+
 </style>
